239a612a5cba3f2c3b49401bf9f8413d5efe129f
eu21 <85627786+eu21-code@users.noreply.github.com>
2022-05-07 11:49:45 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,97 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+ 
+var particles = [];
+var plinkos = [];
+var divisions =[];
+var divisionHeight=300;
+var score =0;
+function setup() {
+  createCanvas(800, 800);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+  //criar objetos de divisão
+  for (var k = 0; k <=80; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  for (var k = 80; k <=160; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  for (var k = 160; k <=240; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  for (var k = 240; k <=320; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  for (var k = 320; k <=400; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  for (var k = 400; k <=480; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  for (var k = 480; k <=560; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  for (var k = 560; k <=640; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  for (var k = 640; k <=720; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+  for (var k = 720; k <=800; k = k + 80) {
+    divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+  }
+
+  //crie a 1ª linha de objetos plinko
+  for (var j = 75; j <=width; j=j+50) { 
+    plinkos.push(new Plinko(j,75));
+  }
+
+  //crie a 2ª linha de objetos plinko
+  for (var j = 50; j <=width-10; j=j+50) 
+  {
+    plinkos.push(new Plinko(j,175));
+  }
+
+  //crie a 3ª linha de objetos plinko
+  for(var j = 75; j <=width; j=j+50){
+    plinkos.push(new Plinko(j,275));
+  }
+  //crie a 4ª linha de objetos plinko
+  for(var j = 50; j <=width-10; j=j+50){
+    plinkos.push(new Plinko(j,375));
+  }
+
+  //criar objetos de partículas
+  if(frameCount%60===0){
+    particles.push(new Particle(random(width/2-10,width/2+10),10,10));
+  }
+  console.log(frameCount);
+}
+ 
+
+
+function draw() {
+  background("black");
+  textSize(20)
+ 
+  Engine.update(engine);
+  ground.display();
+  
+  //exibir os plinkos
+  for (var i = 0; i < plinkos.length; i++) {
+    plinkos[i].display();   
+  }
+   
+  //exibir os divisões
+  for (var k = 0; k < divisions.length; k++) {
+    divisions[k].display();
+  }
+
+  //exibir as partículas
+
+}
